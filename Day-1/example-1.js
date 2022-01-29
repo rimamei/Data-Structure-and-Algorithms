8e9ed418a78a24f17a3c13c4bde89c93b009f632
Rima <rimameih@gmail.com>
2022-01-29 20:52:53 +0700

    example 1

---
+++ Day-1/example-1.js
@@ -0,0 +1,32 @@
+const shoes = ["shoes"];
+const brand = [
+  "adidas",
+  "reebook",
+  "nike",
+  "nuovo",
+  "converse",
+  "vans",
+  "puma",
+  "superga",
+  "league",
+  "piero",
+  "prodo",
+  "amble",
+];
+
+const large = new Array(100000).fill('shoes')
+
+function findShoes(array) {
+  let t0 = performance.now();
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "shoes") {
+      console.log("Found the shoes!");
+    }
+  }
+  let t1 = performance.now();
+  console.log(`Call to find the shoes took ${t1 - t0} ms`);
+}
+
+findShoes(large);
+findShoes(shoes);
+findShoes(brand);
