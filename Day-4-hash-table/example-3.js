d1051dbb04581b4b0b17a22bde5e0dd9790be0c4
Rima <rimameih@gmail.com>
2022-02-01 19:24:54 +0700

    fix first recurring charracter

--- Day-4-hash-table/example-3.js
+++ Day-4-hash-table/example-3.js
@@ -10,13 +10,13 @@
 
 function firstRecurringCharacter(input) {
   for (let i = 0; i < input.length; i++) {
-    for (let j = 0; j < input.length; j++) {
-      if (input[i] === input[j]) {
+    for (let j = i + 1; j < input.length; j++) {
+      if(input[i] === input[j]) {
         return input[i];
       }
     }
   }
-  return undefined;
+  return undefined
 }
 
 function firstRecurringCharacter2(input) {
@@ -31,7 +31,7 @@ function firstRecurringCharacter2(input) {
   return undefined;
 }
 
-console.log(firstRecurringCharacter2([1,2,2,1,5,5,1,3,4,6]));
+console.log(firstRecurringCharacter2([1, 2, 2, 1, 5, 5, 1, 3, 4, 6]));
 
 //Bonus... What if we had this:
 // [2,5,5,2,3,5,1,2,4]
